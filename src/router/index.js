645053def0f332c78de056293dcc595badd322b8
akmot9 <avicocyprien@yahoo.com>
2023-12-05 17:01:18 +0100

    button stop

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeComponent from '../views/homeView.vue'; // Corrected path for App.vue
 import AnalyseView from '../views/analyseView.vue'; // Corrected path for AnalyseView.vue
+import GraphView from '../views/graphView.vue'; // Corrected path for
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+  {
     path: '/', // URL path for HomeComponent
     name: 'home',
     component: HomeComponent
@@ -14,7 +15,12 @@ const router = createRouter({
     path: '/analyse', // URL path for AnalyseView
     name: 'analyseView',
     component: AnalyseView
-  }
+  },
+  {
+    path: '/graph', // URL path for AnalyseView
+    name: 'graphView',
+    component: GraphView
+  },
 ]
 })
 
--- src/views/analyseView.vue
+++ src/views/analyseView.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="analyse-container">
+    <div class="button-container">
+    <button @click="goToNextPage">Vue graphique</button>
+    <button >Stop</button>
+  </div>
     <div class="top-section">
       <Matrice />
       <Stat />
+      <!-- Ajout d'une nouvelle section pour les informations supplémentaires -->
+      <div class="additional-info">
+        <p>Temps de relevé: 00:00:00</p>
+        <p>Trames reçues: 100 / 1000</p>
+        <p>Niveau de confidentialité: DR</p>
+      </div>
     </div>
     <BottomLong />
   </div>
+  
 </template>
 
 <script>
@@ -14,6 +25,15 @@ import Matrice from '../components/CaptureVue/Matrice.vue';
 import Stat from '../components/CaptureVue/Stat.vue';
 
 export default {
+  data() {
+    return {
+      // Initialisation des données
+      tempsReleve: '00:00:00', // Exemple de format, à ajuster selon vos données
+      tramesRecues: 1000,
+      tramesEnregistrees: 100,
+      niveauConfidentialite: 'DR' // Exemple, à ajuster selon vos besoins
+    };
+  },
   components: {
     BottomLong,
     Matrice,
@@ -23,7 +43,13 @@ export default {
     // ...
   },
   methods: {
-    // ...
+    async handleClick() {
+      //console.log(`You clicked on interface: ${netInterface}`);
+      goToNextPage();
+    },
+    goToNextPage() {
+      this.$router.push("/graph");
+    }
   },
   mounted() {
     console.log("analyse mounted");
@@ -35,22 +61,71 @@ export default {
 .analyse-container {
   display: flex;
   flex-direction: column;
-  height: 100vh; /* Utiliser la hauteur de la fenêtre */
+  height: 100vh; /* Full height of the viewport */
+  color: #333; /* Default text color for visibility */
 }
 
 .top-section {
   display: flex;
-  flex-wrap: wrap; /* Permet aux éléments de passer à la ligne si nécessaire */
+  flex-wrap: wrap; /* Wrap items when not enough space */
   justify-content: space-between;
+  padding: 20px; /* Spacing around the inner content */
+  border-bottom: 1px solid #ddd; /* Separator from the rest of the content */
 }
 
 .Matrice, .Stat {
-  flex: 1; /* Répartit l'espace également */
-  min-width: 300px; /* Une largeur minimale pour éviter que les composants ne deviennent trop petits */
+  flex: 1; /* Each takes equal width */
+  min-width: 300px; /* Minimum width so they don't get too narrow */
+  margin: 10px; /* Spacing between components */
+  border: 1px solid #ddd; /* Border for definition */
+  border-radius: 4px; /* Slightly rounded corners */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
 }
 
 .BottomLong {
-  flex-basis: 100%; /* Prend toute la largeur disponible */
+  flex-basis: 100%; /* Takes full width */
+  padding: 20px; /* Spacing around the inner content */
+  position: right ;
+  border-top: 1px solid; /* Separates from the above content */
 }
+
+.button-container {
+  display: flex;
+  justify-content: flex-end; /* Aligns the button to the right */
+  padding: 20px; /* Adds some space around the button */
+}
+
+.button-container button:hover {
+  background-color: #0056b3; /* Darker shade on hover */
+}
+
+.additional-info {
+  padding: 10px;
+  border: 1px solid #ddd;
+  margin-top: 10px;
+  color: chocolate;
+  border-radius: 4px; /* Consistency in design */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* As with Matrice and Stat */
+}
+
+/* Additional styles for the charts if needed */
+.chart {
+  padding: 15px;
+  background: #ffffff;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* Responsive design adjustments */
+@media (max-width: 768px) {
+  .top-section {
+    flex-direction: column; /* Stack the components on smaller screens */
+  }
+
+  .Matrice, .Stat {
+    flex-basis: 100%; /* Full width on smaller screens */
+  }
+}
+
 </style>
 
