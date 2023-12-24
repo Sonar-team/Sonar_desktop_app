9ae4c87ec6eda3550982b665de77ded1ac5b9b83
akmot9 <avicocyprien@yahoo.com>
2023-12-24 19:03:43 +0100

    duplicated methode fix

--- src/components/NavBar/TopBar.vue
+++ src/components/NavBar/TopBar.vue
@@ -1,17 +1,13 @@
 <template>
     <div class="container">
       <div class="sidebar">
-        <p>Temps restant: {{ tempsReleve }}</p>
-        <p>Trames reçues: {{ tramesRecues }} / {{ tramesEnregistrees }}</p>
-        <p>Niveau de confidentialité: {{ niveauConfidentialite }}</p>
+
         
         <!-- Navigation Links -->
         <router-link to="/" class="nav-link">Home</router-link>
         <router-link :to="{ name: 'Analyse' }" class="nav-link">Analyse</router-link>
         <router-link to="/graph" class="nav-link">Graph View</router-link>
   
-        <button @click="stopAndSave">Stop</button>
-        <button @click="goToAnalysePage">Vue analyse</button>
       </div>
   
       <!-- Contenu principal de la page -->
@@ -25,9 +21,6 @@
   export default {
   data() {
     return {
-      tempsReleve: '01:00:00',
-      tramesRecues: 0,
-      tramesEnregistrees: 0,
       niveauConfidentialite: '',
       installationName:''
     };
@@ -35,75 +28,14 @@
   components: {
     NetworkGraphComponent
   },
-  methods: {
-
-    goToAnalysePage() {
-      this.$router.push({
-        name: 'Analyse',
-        params: {
-          netInterface: this.selectedNetInterface,
-          confidentialite: this.confidentialite,
-          installationName: this.installationName,
-          time: this.time,
-        },
-      });
-    }
-  },
-  methods: {
-    goToNextPage() {
-      this.$router.push("/graph");
-    },
-    incrementTramesRecues() {
-      this.tramesRecues++;
-    },
-    incrementMatriceCount() {
-      this.tramesEnregistrees++;
-    },
-
-    getCurrentDate() {
-      // Fonction pour obtenir la date actuelle
-      const now = new Date();
-      // Formattez la date en DD/MM/YYYY
-      const formattedDate = `${this.padZero(now.getDate())}/${this.padZero(now.getMonth() + 1)}/${now.getFullYear()}`;
-      return formattedDate;
-    },
-    updateTempsReleve() {
-      // Fonction pour mettre à jour tempsReleve toutes les secondes
-      setInterval(() => {
-        const timeParts = this.tempsReleve.split(':');
-        let hours = parseInt(timeParts[0]);
-        let minutes = parseInt(timeParts[1]);
-        let seconds = parseInt(timeParts[2]);
-
-        if (seconds > 0) {
-          seconds--;
-        } else {
-          if (minutes > 0) {
-            minutes--;
-            seconds = 59;
-          } else {
-            if (hours > 0) {
-              hours--;
-              minutes = 59;
-              seconds = 59;
-            } else {
-              // Le temps est écoulé, arrêter le timer ici si nécessaire
-            }
-          }
-        }
-        this.tempsReleve = `${this.padZero(hours)}:${this.padZero(minutes)}:${this.padZero(seconds)}`;
-      }, 1000); // Mise à jour chaque seconde (1000 millisecondes)
-    },
-  },
+  
   mounted() {
     console.log("analyse mounted");
     this.updateTempsReleve();
 
-    this.netInterface = this.$route.params.netInterface;
-    this.installationName = this.$route.params.installationName;
-    this.tempsReleve = this.$route.params.time;
-    this.niveauConfidentialite = this.$route.params.confidentialite;
+
   }
+
 }
 </script>
 
