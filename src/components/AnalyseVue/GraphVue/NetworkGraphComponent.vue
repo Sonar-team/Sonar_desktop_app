529dd13996109bd08cdd118aac0bcdae4500ee21
akmot9 <avicocyprien@yahoo.com>
2024-01-31 17:10:03 +0100

    composition sfc for netgraph

--- src/components/AnalyseVue/GraphVue/NetworkGraphComponent.vue
+++ src/components/AnalyseVue/GraphVue/NetworkGraphComponent.vue
@@ -1,24 +1,40 @@
 <!-- YourVueComponent.vue -->
-<script setup lang="ts">
+<script >
   import { VNetworkGraph } from "v-network-graph"
   import { invoke } from '@tauri-apps/api/tauri';
 
   import * as vNG from "v-network-graph"
-  import data from "./data.js"
+  import importedInfo from './data.ts';
 
-  const configs = vNG.defineConfigs({
-  node: {
-    selectable: true,
-    normal: { color: "#E0E0E0" }, // Light grey for visibility on dark background
-    label: { 
-      color: "#E0E0E0",
-      fontSize: 18},   // Same as node color for consistency
+  export default {
+  components: {
+    VNetworkGraph,
   },
-  edge: {
-    selectable: true,
-    normal: { color: "#C0C0C0" }, // Slightly darker grey for distinction but still visible
-  }
-})
+  data() {
+    return {
+      graphData: importedInfo, // Use the imported info here
+
+      packets: [],
+      intervalId: null,
+
+      configs: vNG.defineConfigs({
+        node: {
+          selectable: true,
+          normal: { color: "#E0E0E0" }, // Light grey for visibility on dark background
+          label: { 
+            color: "#E0E0E0",
+            fontSize: 18},   // Same as node color for consistency
+        },
+        edge: {
+          selectable: true,
+          normal: { color: "#C0C0C0" }, // Slightly darker grey for distinction but still visible
+        }
+      })
+    };
+  },
+}
+
+
 
 </script>
 
@@ -26,9 +42,9 @@
 
   <v-network-graph
     class="graph"
-    :nodes="data.nodes"
-    :edges="data.edges"
-    :layouts="data.layouts"
+    :nodes="graphData.nodes"
+    :edges="graphData.edges"
+    :layouts="graphData.layouts"
     :configs="configs"
 
   />
