24027f97ca8f49700f291002b6663be78da7cf03
akmot9 <avicocyprien@yahoo.com>
2024-02-15 10:02:23 +0100

    mod wind for interface

--- src-tauri/src/get_interfaces/mod.rs
+++ src-tauri/src/get_interfaces/mod.rs
@@ -7,16 +7,17 @@ use pnet::datalink;
 /// pour représenter la sélection de toutes les interfaces.
 ///
 /// Cette fonction se sert de `pnet::datalink::interfaces` pour obtenir une liste
-/// de toutes les interfaces réseau disponibles sur le système courant. Elle parcourt ensuite ces
-/// interfaces, récupérant leurs noms dans un vecteur. Enfin, elle ajoute une chaîne
-/// "Toutes les interfaces" à ce vecteur, permettant de représenter l'option de
-/// choisir toutes les interfaces dans une interface utilisateur ou un paramètre de configuration.
+/// de toutes les interfaces réseau disponibles sur le système courant. Pour Linux,
+/// elle retourne les noms d'interface et pour Windows, elle retourne les adresses MAC
+/// des interfaces. Enfin, elle ajoute une chaîne "Toutes les interfaces" à ce vecteur,
+/// permettant de représenter l'option de choisir toutes les interfaces dans une interface
+/// utilisateur ou un paramètre de configuration.
 ///
 /// # Retours
-/// 
-/// Un `Vec<String>` contenant les noms de toutes les interfaces réseau trouvées sur le système,
-/// plus une entrée "Toutes les interfaces" indiquant l'option de sélection de
-/// toutes les interfaces.
+///
+/// Un `Vec<String>` contenant les noms ou adresses MAC de toutes les interfaces réseau
+/// trouvées sur le système, plus une entrée "Toutes les interfaces" indiquant l'option
+/// de sélection de toutes les interfaces.
 ///
 /// # Exemples
 ///
@@ -34,14 +35,20 @@ pub fn get_interfaces() -> Vec<String> {
     // Récupère une liste de toutes les interfaces réseau via le module datalink de pnet.
     let interfaces = datalink::interfaces();
     // Log l'action de récupération des interfaces réseau.
-    info!("récupération des interfaces réseau");
+    info!("Récupération des interfaces réseau");
 
-    // Mappe les interfaces à leurs noms, les collectant dans un vecteur.
+    // Mappe les interfaces à leurs noms ou adresses MAC, les collectant dans un vecteur.
     let mut names: Vec<String> = interfaces
         .iter()
         .map(|iface| {
-            // Utilise l'adresse MAC comme nom de l'interface.
-            format!("Interface MAC: {}", iface.mac.unwrap_or_default())
+            #[cfg(target_os = "linux")]
+            {
+                iface.name.clone() // Retourne le nom de l'interface sous Linux.
+            }
+            #[cfg(target_os = "windows")]
+            {
+                format!("Interface MAC: {}", iface.mac.unwrap_or_default()) // Retourne l'adresse MAC de l'interface sous Windows.
+            }
         })
         .collect();
  
@@ -60,12 +67,14 @@ mod tests {
 
     #[test]
     fn test_get_interfaces() {
-
         // Appelle la fonction à tester.
         let interface_names = get_interfaces();
 
         // Vérifie que le vecteur de noms d'interface n'est pas vide.
         assert!(!interface_names.is_empty());
+
+        #[cfg(target_os = "windows")]
+        assert!(interface_names.iter().any(|name| name.starts_with("Interface MAC: ")));
 
         // Vérifie que le dernier élément du vecteur est "Toutes les interfaces".
         assert_eq!(interface_names.last(), Some(&String::from("Toutes les interfaces")));
