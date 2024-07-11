338846b8c4ff07ef0e9eabb939e81ed2e69f0367
akmot9 <avicocyprien@yahoo.com>
2024-07-11 12:40:42 +0200

    fix profinet-rt

--- src-tauri/src/sniff/capture_packet/layer_2_infos/layer_3_infos/profinet.rs
+++ src-tauri/src/sniff/capture_packet/layer_2_infos/layer_3_infos/profinet.rs
@@ -73,22 +73,22 @@ impl ProfinetPacket {
             return None;
         }
 
-        let frame_id_value = u16::from_be_bytes([data[12], data[13]]);
+        let frame_id_value = u16::from_be_bytes([data[0], data[1]]);
         println!("Frame ID value: {:04x}", frame_id_value);
         let frame_id = FrameId::from_u16(frame_id_value)?;
         println!("Parsed Frame ID: {:?}", frame_id);  // Debug trace
-        let cycle_counter = u16::from_be_bytes([data[14], data[15]]);
+        let cycle_counter = u16::from_be_bytes([data[2], data[3]]);
         println!("Cycle Counter: {}", cycle_counter);
-        let data_status = match data[16] {
+        let data_status = match data[4] {
             0x80 => DataStatus::Good,
             0x00 => DataStatus::Bad,
             _ => {
-                println!("Invalid data status: {:02x}", data[16]);
+                println!("Invalid data status: {:02x}", data[4]);
                 return None;
             }
         };
         println!("Data Status: {:?}", data_status);
-        let transfer_status = match data[17] {
+        let transfer_status = match data[5] {
             0x00 => TransferStatus::Stopped,
             0x01 => TransferStatus::Running,
             value => {
@@ -98,7 +98,7 @@ impl ProfinetPacket {
         };
         println!("Transfer Status: {:?}", transfer_status);  // Debug trace
 
-        let user_data = data[18..].to_vec();
+        let user_data = data[6..].to_vec();
         println!("User Data: {:?}", user_data);  // Debug trace
 
         Some(ProfinetPacket {
