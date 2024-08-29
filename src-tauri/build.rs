6f75b852c29de371bac55d9377467ffedb340bcc
Akmot9 <avicocyprien@yahoo.com>
2024-08-29 08:20:16 +0200

    ressource lib win

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tauri_build::build()
+    // Tauri build process
+    tauri_build::build();
+    
+    // Only add the Packet.lib library on Windows
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-search=native=lib");
+        println!("cargo:rustc-link-lib=static=Packet");
+    }
 }
