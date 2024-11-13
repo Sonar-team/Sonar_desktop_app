938a52e666e61e2e9535d5f280a9076cac903b9b
akmot9 <avicocyprien@yahoo.com>
2024-11-13 09:15:20 +0100

    acc local lib to fasten buil on windows

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,7 +5,8 @@ fn main() {
     // Only add the Packet.lib library on Windows
     #[cfg(target_os = "windows")]
     {
-        println!("cargo:rustc-link-search=native=lib");
+        println!("cargo:rustc-link-search=native=./lib");
         println!("cargo:rustc-link-lib=static=Packet");
+        println!("cargo:rustc-link-lib=static=wpcap");
     }
 }
