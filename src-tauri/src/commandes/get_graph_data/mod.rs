f914b5fc28e1a2732f37db6ef10ecfd1246f81c9
cyprien avico <avicocyprien@yahoo.com>
2025-06-23 19:09:11 +0200

    last proto graph cow

--- src-tauri/src/commandes/get_graph_data/mod.rs
+++ src-tauri/src/commandes/get_graph_data/mod.rs
@@ -1,44 +1,15 @@
 use serde::Serialize;
+use std::borrow::Cow;
 use std::collections::HashMap;
 
 use crate::tauri_state::{
     capture::capture_handle::layer_2_infos::layer_3_infos::ip_type::IpType, matrice::PacketKey,
 };
 
-/// Récupère et sérialise les données de trafic réseau en une représentation de graph.
-///
-/// Cette fonction tente d'acquérir un verrou sur l'état partagé contenant les informations des paquets
-/// et sérialise ces données en une chaîne JSON. Cela permet une transmission facile des données
-/// pour la visualisation ou l'analyse ultérieure.
-///
-/// # Structures
-///
-/// `GraphData` représente les données de graph avec des nœuds et des arêtes.
-/// `Node` représente un nœud dans le graph.
-/// `Edge` représente une arête entre deux nœuds dans le graph.
-/// `GraphBuilder` est utilisé pour construire les données de graph.
-///
-/// # Fonctionnement
-///
-/// La fonction `get_graph_data` construit les données de graph à partir des paquets de trafic réseau
-/// et les sérialise en JSON.
-///
-/// # Exemple
-///
-/// Supposons que vous ayez un état partagé `shared_state` initialisé et passé à cette fonction :
-///
-/// ```ignore
-/// let result = get_graph_data(shared_state);
-/// match result {
-///     Ok(json_string) => println!("Données sérialisées : {}", json_string),
-///     Err(e) => eprintln!("Erreur : {}", e),
-/// }
-/// ```
-
 #[derive(Serialize)]
-pub struct GraphData {
+pub struct GraphData<'a> {
     nodes: HashMap<String, Node>,
-    edges: HashMap<String, Edge>,
+    edges: HashMap<String, Edge<'a>>,
 }
 
 #[derive(Serialize, Clone)]
@@ -47,20 +18,21 @@ struct Node {
     color: String,
     mac: String,
 }
+
 #[derive(Serialize, Clone)]
-struct Edge {
+struct Edge<'a> {
     source: String,
     target: String,
-    label: String,
+    label: Cow<'a, str>,
 }
 
-pub struct GraphBuilder {
+pub struct GraphBuilder<'a> {
     nodes: HashMap<String, Node>,
-    edges: HashMap<String, Edge>,
+    edges: HashMap<String, Edge<'a>>,
     edge_counter: u32,
 }
 
-impl GraphBuilder {
+impl<'a> GraphBuilder<'a> {
     pub fn new() -> Self {
         GraphBuilder {
             nodes: HashMap::new(),
@@ -69,87 +41,71 @@ impl GraphBuilder {
         }
     }
 
-    // Function to determine the color of a node based on its IP type
     fn determine_color(ip_type: &Option<IpType>) -> String {
         match ip_type {
-            Some(IpType::Private) => "#D4D3DC".to_string(), // Light gray for private IPs
-            Some(IpType::Public) => "#317AC1".to_string(),  // Blue for public IPs
-            Some(IpType::Multicast) => "#FF5733".to_string(), // Red for multicast IPs
-            Some(IpType::Loopback) => "#FF5733".to_string(), // Red for loopback IPs    
-            Some(IpType::Unknown) => "#FF5733".to_string(), // Red for unknown IPs  
-            Some(IpType::Apipa) => "#FF5733".to_string(), // Red for APIPA IPs  
-            Some(IpType::LinkLocal) => "#FF5733".to_string(), // Red for link-local IPs  
-            Some(IpType::Ula) => "#FF5733".to_string(), // Red for ULA IPs  
-  
-            _ => "#FF5733".to_string(), // Red for others (e.g., multicast or loopback, if they ever get through)
+            Some(IpType::Private) => "#D4D3DC".to_string(),
+            Some(IpType::Public) => "#317AC1".to_string(),
+            Some(
+                IpType::Multicast
+                | IpType::Loopback
+                | IpType::Unknown
+                | IpType::Apipa
+                | IpType::LinkLocal
+                | IpType::Ula,
+            ) => "#FF5733".to_string(),
+            _ => "#FF5733".to_string(),
         }
     }
 
-    // Fonction pour vérifier si une arête existe déjà
-    pub fn edge_exists(&self, source_ip: &String, target_ip: &String, label: &String) -> bool {
+    pub fn edge_exists(&self, source_ip: &String, target_ip: &String, label: &str) -> bool {
         self.edges.values().any(|e| {
-            (e.source == *source_ip && e.target == *target_ip && e.label == *label)
-                || (e.source == *target_ip && e.target == *source_ip && e.label == *label)
+            (e.source == *source_ip && e.target == *target_ip && e.label == label)
+                || (e.source == *target_ip && e.target == *source_ip && e.label == label)
         })
     }
 
-    /// Adds an edge to the graph based on packet information if the source and target IPs are
-    /// either private or public IPv4 addresses.
-    ///
-    /// This function first checks if both the source and target IP addresses in the packet
-    /// are either private or public. If so, it determines the color for each node based on
-    /// their IP type. It ensures that nodes for the source and target IPs are present in the
-    /// graph, creating them if necessary. An edge is then added between these nodes if it
-    /// does not already exist, using the layer 3 protocol as the label.
-    ///
-    /// # Arguments
-    ///
-    /// * `packet` - A reference to `PacketKey` containing information about the network packet,
-    ///   including source and target IP addresses, their types, and MAC addresses.
-    pub fn add_edge(&mut self, packet: &PacketKey) {
+    pub fn add_edge(&mut self, packet: &'a PacketKey) {
         if let (Some(source_ip), Some(target_ip)) = (
             &packet.layer_3_infos.ip_source,
             &packet.layer_3_infos.ip_destination,
         ) {
-            let is_source_ip_private_or_public_ipv4 = matches!(
+            let is_source_ip_valid = matches!(
                 packet.layer_3_infos.ip_source_type,
                 Some(IpType::Private | IpType::Public)
             );
-            let is_target_ip_private_or_public_ipv4 = matches!(
+            let is_target_ip_valid = matches!(
                 packet.layer_3_infos.ip_destination_type,
                 Some(IpType::Private | IpType::Public)
             );
 
-            if is_source_ip_private_or_public_ipv4 && is_target_ip_private_or_public_ipv4 {
-                // Use IP type to determine color
+            if is_source_ip_valid && is_target_ip_valid {
                 let source_color = Self::determine_color(&packet.layer_3_infos.ip_source_type);
                 let target_color = Self::determine_color(&packet.layer_3_infos.ip_destination_type);
 
-                // Ensure nodes for source and target IPs are created
                 self.nodes.entry(source_ip.clone()).or_insert_with(|| Node {
                     name: source_ip.clone(),
                     color: source_color,
                     mac: packet.mac_address_source.clone(),
                 });
+
                 self.nodes.entry(target_ip.clone()).or_insert_with(|| Node {
                     name: target_ip.clone(),
                     color: target_color,
                     mac: packet.mac_address_destination.clone(),
                 });
 
-                // Determine the label from protocol information
-                let label = if let Some(protocol) = packet.layer_3_infos.layer_4_infos.l_7_protocol.as_deref() {
-                    protocol
-                } else if let Some(protocol) = packet.layer_3_infos.l_4_protocol.as_deref() {
-                    protocol
-                } else if let Some(protocol) = packet.layer_3_infos.l_4_protocol.as_deref() {
-                    protocol
+                let label = if let Some(protocol) =
+                    packet.layer_3_infos.layer_4_infos.l_7_protocol.as_deref()
+                {
+                    Cow::Borrowed(protocol)
+                } else if let Some(protocol) =
+                    packet.layer_3_infos.l_4_protocol.as_deref()
+                {
+                    Cow::Borrowed(protocol)
                 } else {
-                    &packet.l_3_protocol
+                    Cow::Borrowed(packet.l_3_protocol.as_str())
                 };
-                let label: String = label.to_string();
 
-                // Add edge if it doesn't exist yet
                 if !self.edge_exists(source_ip, target_ip, &label) {
                     let edge_name = format!("edge{}", self.edge_counter);
                     self.edges.insert(
@@ -157,7 +113,7 @@ impl GraphBuilder {
                         Edge {
                             source: source_ip.clone(),
                             target: target_ip.clone(),
-                            label: label.clone(),
+                            label,
                         },
                     );
                     self.edge_counter += 1;
@@ -166,15 +122,10 @@ impl GraphBuilder {
         }
     }
 
-    pub fn build_graph_data(&self) -> GraphData {
+    pub fn build_graph_data(&self) -> GraphData<'a> {
         GraphData {
             nodes: self.nodes.clone(),
             edges: self.edges.clone(),
         }
     }
 }
-
-// Helper function to determine if an IP address is IPv4
-// fn is_ipv4(ip: &String) -> bool {
-//     ip.contains('.') && !ip.contains(':')
-// }
